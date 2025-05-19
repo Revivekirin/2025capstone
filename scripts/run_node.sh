136ce0fd3f5040a6513aec0b897894f4473e6d40
Revivekirin <kimwiz4352@gmail.com>
2025-05-19 17:59:39 +0900

    Edit page traversal

--- scripts/run_node.sh
+++ scripts/run_node.sh
@@ -2,6 +2,7 @@
 echo "[$(date)] 📦 node-crawler 실행 시작"
 
 cd /home/capstone-design/Downloads/2025capstone || exit 1
+# cd /Users/kimjihe/Desktop/git/2025capstone || exit 1
 
 # Docker 이미지 빌드 및 실행 
 docker-compose up -d node-crawler
