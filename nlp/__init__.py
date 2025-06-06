ab6d6a4ca3dff63c3a29a3e26e87a683ea12126a
Revivekirin <kimwiz4352@gmail.com>
2025-06-07 01:12:17 +0900

    Fix error

--- nlp/__init__.py
+++ nlp/__init__.py
@@ -1,6 +1,7 @@
 from .translator import translate_boannews_with_argos
 from .embedder import get_segmented_embedding
 from .summarize import add_summaries_to_articles
+from .embedder_st import get_embedding_st
 
 # __all__ = [
 #     'process_articles_for_translation',
